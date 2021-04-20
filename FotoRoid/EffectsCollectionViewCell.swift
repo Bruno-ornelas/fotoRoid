057c5604917bc53de1a1ee195b0b6745d7af8079
Bruno Ornelas <brunoornelas@MacBook-Pro-de-Bruno.local>
2021-04-20 15:26:23 -0300

    first

---
+++ FotoRoid/EffectsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  EffectsCollectionViewCell.swift
+//  FotoRoid
+//
+//  Created by Bruno Ornelas on 19/10/20.
+//
+
+import UIKit
+
+class EffectsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ FotoRoid/FilterManager.swift
@@ -0,0 +1,8 @@
+//
+//  FilterManager.swift
+//  FotoRoid
+//
+//  Created by Bruno Ornelas on 17/10/20.
+//
+
+import Foundation
